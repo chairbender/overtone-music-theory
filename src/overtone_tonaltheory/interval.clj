a8347537fa45459c84f6519a60466c6dd29a5056
Kyle Hipke <kwhipke1@gmail.com>
2014-05-07 21:40:04 -0700

    added back some stuff that was accidentally deleted

--- src/overtone_tonaltheory/interval.clj
+++ src/overtone_tonaltheory/interval.clj
@@ -3,26 +3,26 @@
   (:require [clojure.math.numeric-tower :as math])
   (:require [overtone.core :as overtone]))
 
-;A vector of sets of enharmonically equivalent intervals (represented as keywords) in order from smallest to largest, ranging
-;from unison to major 10th. The array index of an interval keyword set is equivalent to the
+;A vector of vectors of enharmonically equivalent intervals (represented as keywords) in order from smallest to largest, ranging
+;from unison to major 10th. The array index of an enharmonic group is equivalent to the
 ;number of semitones that are spanned by the intervals in that set.
-(def ^{:private true} intervals-by-enharmonic [#{:unison}
-                #{:m2}
-                #{:maj2}
-                #{:m3}
-                #{:maj3 :dim4}
-                #{:4}
-                #{:aug4 :dim5}
-                #{:5}
-                #{:aug5 :m6}
-                #{:maj6 :dim7}
-                #{:aug6 :m7}
-                #{:maj7}
-                #{:octave}
-                #{:augoctave :m9}
-                #{:maj9}
-                #{:m10}
-                #{:maj10}])
+(def ^{:private true} intervals-by-enharmonic [[:unison]
+                [:m2]
+                [:maj2]
+                [:m3]
+                [:maj3 :dim4]
+                [:4]
+                [:aug4 :dim5]
+                [:5]
+                [:aug5 :m6]
+                [:maj6 :dim7]
+                [:aug6 :m7]
+                [:maj7]
+                [:octave]
+                [:augoctave :m9]
+                [:maj9]
+                [:m10]
+                [:maj10]])
 
 ;A map of each interval to the number of semitones it contains
 (def ^{:private true} intervals-to-semitones
@@ -37,17 +37,28 @@
 ;The number of semitones above which intervals will be treated as compound intervals.
 (def ^{:private true} compound-interval-cutoff 16)
 
+(defn- interval-distance
+  "Returns the absolute distance in semitones between two midi notes"
+  [note otherNote]
+  (math/abs (- (midi->semitones note) (midi->semitones otherNote))))
+
+(defn- interval-semitones
+	"Returns the number of semitones in the given interval (keyword) spans."
+	[interval]
+	(intervals-to-semitones interval))
+
+
 
 (defn interval-keywords
-  "Returns a vector containing sets of the keywords that represent the interval between the two notes.
+  "Returns a vector containing subvectors of the keywords that represent the interval between the two notes.
   The two notes must be in midi note format.
-  The individual sets consist of the enharmonically-equivalent keys for the interval (for example, a
-  set containing major 3rd and diminished 4th), while the vector contains those interval-sets.
+  The individual subvectors consist of the enharmonically-equivalent keys for the interval (for example, a
+  subvector containing major 3rd and diminished 4th), while the vector contains those subvectors.
   If the octave is larger than a major 10th, the vector will contain the intervals that comprise
   that compound interval, ordered from greatest to least interval. In the case of compound intervals,
   only intervals of an octave or less will be used to describe the interval.
 
-  For example, if the interval between the notes is an eleventh, [{:octave}, {:4}] will be returned
+  For example, if the interval between the notes is an eleventh, [[:octave], [:4] will be returned
   (representing an octave and a fourth).
 
   The interval keywords follow a form similar to other overtone keywords:
--- test/overtone_tonaltheory/t_interval.clj
+++ test/overtone_tonaltheory/t_interval.clj
@@ -6,19 +6,19 @@
 (facts "about `interval-keywords`"
   (fact "For intervals up to a major tenth, it returns a vector of a single set
         that contains the keywords describing those intervals"
-    (interval-keywords :A4 :B4) => [#{:maj2}]
-    (interval-keywords :A4 :C4) => [#{:m3}]
-    (interval-keywords :A3 :A3) => [#{:unison}]
-    (interval-keywords :A3 :A4) => [#{:octave}]
-    (interval-keywords :A3 :C#4) => [#{:maj10}]
-    (interval-keywords :D4 :F#4) => [#{:maj3 :dim4}])
+    (interval-keywords :A4 :B4) => [[:maj2]]
+    (interval-keywords :A4 :C4) => [[:m3]]
+    (interval-keywords :A3 :A3) => [[:unison]]
+    (interval-keywords :A3 :A4) => [[:octave]]
+    (interval-keywords :A3 :C#4) => [[:maj10]]
+    (interval-keywords :D4 :F#4) => [[:maj3 :dim4]])
 
   (fact "For intervals greater than a major tenth, it returns a vector
         of the intervals making up the compound interval, with octaves
         being the highest interval possible and comingfirst"
-    (interval-keywords :A3 :D4) => [#{:octave} #{:4}]
-    (interval-keywords :A3 :A5) => [#{:octave} #{:octave}])
-    (interval-keywords :D3 :E5) => [#{:octave} #{:octave} #{:maj2}])
+    (interval-keywords :A3 :D4) => [[:octave] [:4]]
+    (interval-keywords :A3 :A5) => [[:octave] [:octave]])
+    (interval-keywords :D3 :E5) => [[:octave] [:octave] [:maj2]])
 
 
 (facts "about `note-interval-above'"
